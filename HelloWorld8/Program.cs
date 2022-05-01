4234c59be527f1c7c2714bdc22ece85a19b8388f
ROXANA lazarescu <roxana_lazarescu13@yahoo.com>
2022-05-01 16:48:31 +0300

    Add project files.

---
+++ HelloWorld8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HelloWorld8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          //daca a divizibil cu b, atunci restul este 0;
+          //ALTFEL ESTE INDIVIZIBIL
+            Console.WriteLine("a");
+            int a = 10;
+            Console.WriteLine("b");
+            int b = 5;
+            if(a%b==0)
+            {
+                Console.WriteLine("divizibil");
+
+            }
+            else
+            { 
+                Console.WriteLine("INDIVIZIBIL");
+
+            }
+
+        }
+    }
+}
+
